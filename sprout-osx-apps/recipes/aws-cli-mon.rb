9ab76121d81da8ff291cb3caa13dc493e1426082
Dale <daleki@gmail.com>
2013-10-23 15:01:43 -0700

    add directory

--- sprout-osx-apps/recipes/aws-cli-mon.rb
+++ sprout-osx-apps/recipes/aws-cli-mon.rb
@@ -1,6 +1,11 @@
 include_recipe "sprout-osx-apps::java"
 include_recipe 'sprout-osx-base::bash_it'
 
+directory "/Users/#{node['current_user']}/aws-cli/mon" do
+  recursive true
+  owner node['current_user']
+end
+
 remote_file "/tmp/CloudWatch.zip" do
   source "http://ec2-downloads.s3.amazonaws.com/CloudWatch-2010-08-01.zip"
   action :create_if_missing
